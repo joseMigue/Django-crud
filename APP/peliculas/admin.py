3e40df053489206c406f7e2db847ad6f6d29239c
jose <[jose_aguilar_14@hotmail.com]>
2021-01-04 22:48:46 -0300

    agregar modelos

--- APP/peliculas/admin.py
+++ APP/peliculas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Comentario,Genero,Pelicula,Usuario
 # Register your models here.
+admin.site.register(Comentario)
+admin.site.register(Genero)
+admin.site.register(Pelicula)
+admin.site.register(Usuario)
--- APP/peliculas/models.py
+++ APP/peliculas/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Genero(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Pelicula(models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+    genero = models.ManyToManyField(Genero)
+    fecha_estreno = models.DateTimeField(auto_now_add=True)
+    sinopsis = models.TextField()
+
+    def __str__(self):
+        """Unicode representation of Pelicula."""
+        return self.nombre
+
+class Usuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    fecha_de_nacimiento = models.DateField(auto_now=False,auto_now_add=True)
+    favoritos = models.ManyToManyField(Pelicula)
+    bloqueado = models.BooleanField(default=False)
+
+class Comentario(models.Model):
+    texto = models.CharField(max_length=255)
+    fecha = models.DateField(auto_now=False,auto_now_add=True)
+    pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
