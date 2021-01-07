26a2bf940594f80a068e7874cd80d3d26e7edbac
jose <[jose_aguilar_14@hotmail.com]>
2021-01-07 16:49:18 -0300

    permisos en peliculas

--- APP/peliculas/views.py
+++ APP/peliculas/views.py
@@ -5,30 +5,36 @@ from django.urls import reverse_lazy
 from .forms import PeliculaForm, UsuarioForm,User
 from django.forms import forms
 from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 
 # Create your views here.
-class PeliculaCreateView(CreateView):
+class PeliculaCreateView(PermissionRequiredMixin,LoginRequiredMixin,CreateView):
     model = Pelicula
     form_class = PeliculaForm
+    permission_required = 'peliculas.add_pelicula'
     template_name = "pelicula/pelicula_form.html"
     success_url = reverse_lazy('pelicula:lista')
 
-class PeliculaListView(ListView):
+class PeliculaListView(PermissionRequiredMixin,LoginRequiredMixin,ListView):
     model = Pelicula
+    permission_required = 'peliculas.view_pelicula'
     template_name = "pelicula/pelicula_lista.html"
     
-class PeliculaDetailView(DetailView):
+class PeliculaDetailView(PermissionRequiredMixin,LoginRequiredMixin,DetailView):
     model = Pelicula
+    permission_required = 'peliculas.view_pelicula'
     template_name = "pelicula/pelicula_detalle.html"
      
-class PeliculaDeleteView(DeleteView):
+class PeliculaDeleteView(PermissionRequiredMixin,LoginRequiredMixin,DeleteView):
     model = Pelicula
     form_class = PeliculaForm
     success_url = reverse_lazy('pelicula:lista')
+    permission_required = 'peliculas.delete_pelicula'
 
-class PeliculaUpdateView(UpdateView):
+class PeliculaUpdateView(PermissionRequiredMixin,LoginRequiredMixin,UpdateView):
     model = Pelicula
     form_class = PeliculaForm
+    permission_required = 'peliculas.change_pelicula'
     template_name = "pelicula/pelicula_form.html"
     success_url = reverse_lazy('pelicula:lista')
     
